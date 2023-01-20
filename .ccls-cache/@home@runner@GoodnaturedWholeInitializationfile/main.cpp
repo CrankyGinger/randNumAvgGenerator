e595c654ab9313069ef69223b902ca9183fa0813
Robert Flath <93940336+CrankyGinger@users.noreply.github.com>
2023-01-20 06:26:55 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@GoodnaturedWholeInitializationfile/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <ios>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+  srand(time(0));
+  double randNum1;
+    randNum1 = rand();
+  randNum1 = rand() % 100 + 1;
+  double randNum2;
+    randNum2 = rand();
+  randNum2 = rand() % 100 + 1;
+  double randNum3;
+    randNum3 = rand();
+  randNum3 = rand() % 100 + 1;
+  double randNum4;
+    randNum4 = rand();
+  randNum4 = rand() % 100 + 1;
+  double randNum5;
+    randNum5 = rand();
+  randNum5 = rand() % 100 + 1;
+  double average;
+    average = (randNum1 + randNum2 + randNum3 + randNum4 + randNum5) / 5;
+
+  cout << "Random numbers between 1 and 100: " << endl;
+  cout << randNum1 << endl;
+  cout << randNum2 << endl;
+  cout << randNum3 << endl;
+  cout << randNum4 << endl;
+  cout << randNum5 << endl;
+  cout << "The average is: " << std::fixed <<  std::setprecision(2) << average << endl;
+  
+  return 0;
+}
